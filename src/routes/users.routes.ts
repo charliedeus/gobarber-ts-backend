20585b152a5cc7eee6646f8228fe9630dc2dff75
Charles Loureiro de Deus <charliedeus@gmail.com>
2020-04-22 23:05:22 -0300

    Encrypt password

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -18,6 +18,8 @@ usersRouter.post('/', async (request, response) => {
       password,
     });
 
+    delete user.password;
+
     return response.json(user);
   } catch (error) {
     return response.status(400).json({ error: error.message });
