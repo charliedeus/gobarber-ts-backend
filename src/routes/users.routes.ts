e387bd60fc0c0d6ebc02d320b1d59b4d0412870b
Charles Loureiro de Deus <charliedeus@gmail.com>
2020-04-22 22:57:15 -0300

    Create users

---
+++ src/routes/users.routes.ts
@@ -0,0 +1,27 @@
+import { Router } from 'express';
+
+import CreateUserService from '../services/CreateUserService';
+
+const usersRouter = Router();
+
+// Responsabilidades da Rota: Receber a requisição, chamar outro arquivo e devolver uma resposta
+
+usersRouter.post('/', async (request, response) => {
+  try {
+    const { name, email, password } = request.body;
+
+    const createUser = new CreateUserService();
+
+    const user = await createUser.execute({
+      name,
+      email,
+      password,
+    });
+
+    return response.json(user);
+  } catch (error) {
+    return response.status(400).json({ error: error.message });
+  }
+});
+
+export default usersRouter;
