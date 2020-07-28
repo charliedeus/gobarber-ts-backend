11b0bd2f0ea5a3ccf9d92736617fa8eabee09e95
Charles Loureiro de Deus <charliedeus@gmail.com>
2020-07-28 15:02:06 -0300

    Correcting forgot and reset routes

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -10,7 +10,7 @@ import IUsersRepository from '@modules/users/repositories/IUsersRepository';
 import UsersRepository from '@modules/users/infra/typeorm/repositories/UsersRepository';
 
 import IUserTokensRepository from '@modules/users/repositories/IUserTokensRepository';
-import UserTokenRepository from '@modules/users/infra/typeorm/repositories/UserTokenRepository';
+import UserTokensRepository from '@modules/users/infra/typeorm/repositories/UserTokenRepository';
 
 import INotificationsRepository from '@modules/notifications/repositories/INotificationsRepository';
 import NotificationsRepository from '@modules/notifications/infra/typeorm/repositories/NotificationsRepository';
@@ -26,8 +26,8 @@ container.registerSingleton<IUsersRepository>(
 );
 
 container.registerSingleton<IUserTokensRepository>(
-  'UserTokenRepository',
-  UserTokenRepository,
+  'UserTokensRepository',
+  UserTokensRepository,
 );
 
 container.registerSingleton<INotificationsRepository>(
